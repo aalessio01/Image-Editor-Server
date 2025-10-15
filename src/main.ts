78c6d4cfef8d749d548cca7734f30e5df6c393c2
aalessio01 <alessio.armenio@axelspringer.com>
2025-10-15 09:48:35 +0200

    creating server on main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("start");
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/imageeditorapi/health", (req: Request, res: Response) => {
+  res.status(200).send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`🖼️  Image-Editor-Server listening on http://localhost:${PORT}`);
+});
